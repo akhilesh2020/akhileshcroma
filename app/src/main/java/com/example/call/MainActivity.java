1d811cb400352ff6ca1ca52e822a1039c441a901
akhilesh2020 <itakhilesh2020@gmail.com>
2020-01-02 17:01:48 +0530

    message

---
+++ app/src/main/java/com/example/call/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.call;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+    final int REQUEST_CODE=100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText=(EditText)findViewById(R.id.ed);
+        button=(Button)findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},REQUEST_CODE);
+                }
+                String nuber=editText.getText().toString();
+                Intent intent1=new Intent(Intent.ACTION_CALL);
+                intent1.setData(Uri.parse("tel:"+nuber));
+                startActivity(intent1);
+            }
+        });
+    }
+}
